66fa82ec7d4467e02e4fdbf4e1ffb55a772a901c
Youssef El ghazzali <youssefelghazzali@MacBook-Pro-de-Youssef-2.local>
2024-06-10 00:39:56 -0400

    dernier commit

---
+++ Downloads/Museum-museumVersion4 2/components/Expositions.jsx
@@ -0,0 +1,61 @@
+import styles from './Expositions.module.css'
+import Gallerie from '@/components/Gallerie.jsx'
+import Image from 'next/image';
+import gallerieMusee from '@/public/gallerie-musee.json'
+export default function Expositions() {
+    return (
+        <section className={styles.exposition}>
+            {/*----------------------------ici se le haut d ma page -------------------- */}            
+            <section className={styles.expositionTop}>
+                <div className="overlay"></div>
+                <div className={styles.videoContent}>
+                    <video controls className={styles.video} autoPlay muted loop >
+                        <source src="/Assests/video1.mp4"  type="video/mp4" />
+                        Votre navigateur ne supporte pas la balise vidéo.
+                    </video>
+                </div>
+                <div className={styles.expoContentTop}>
+                    <div className={styles.textDiv}>
+                        <span className={styles.Bienvenue}>
+                            BIENVENUE SUR NOS EXPOSITIONS ÉVÉNEMENTIELLES
+                        </span>
+                        <h2 className=  {styles.expoTitle}>LA COLLECTION SPECIALE DE CE MOIS</h2>
+                    </div>
+                    <div className={styles.icons}>
+                        <h3 className='titleIcons'>ici se trouve les catégories</h3>
+                        <ul className='listeIcons'>
+                            <li> Art</li>
+                            <li> Design</li>
+                            <li>Status</li>
+                        </ul>
+                    </div>
+                </div >
+                
+            </section>
+            {/* ------------------------------- ci se le milieu d ma page------------------------------ */}
+            <section className={styles.expositionMidle}>
+                <div className={styles.galleriesContent}>
+                    { 
+                    <Gallerie images={gallerieMusee} />
+                    }
+                </div>
+                <div className={styles.text}>
+                    <p className={styles.mesParagraphes}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem perferendis recusandae explicabo officiis omnis distinctio? Fugiat vitae debitis officiis, placeat, delectus et saepe cum, molestiae dignissimos ipsum quibusdam provident esse!</p>
+                  <div className={styles.expoContent}>
+                    <div className={styles.textDiv}>
+                        <span className='smallText'>
+                            EXPOSITION ÉVÉNEMENTIELLE
+                        </span>
+                        <h2 className=  {styles.expoTitle}>LA COLLECTION</h2>
+                    </div>
+                </div >
+                <p className={styles.mesParagraphes}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem perferendis recusandae explicabo officiis omnis distinctio? Fugiat vitae debitis officiis, placeat, delectus et saepe cum, molestiae dignissimos ipsum quibusdam provident esse!</p>
+                </div>
+              
+            </section>
+            
+            
+        </section>
+        
+    );
+}
---
+++ Downloads/Museum-museumVersion4 2/docs/preparation.md
@@ -0,0 +1,60 @@
+# Préparation du Projet : Art et Histoire
+
+## Description du Projet
+
+Le projet consiste à créer un site web pour le Musée Art et Histoire. Le site aura pour objectif de présenter les expositions en cours, de fournir des informations sur le musée, et de permettre aux visiteurs de contacter l'administration pour toute question ou demande. Le site sera moderne, facile à naviguer et adapté à tous les appareils (responsive design).
+
+## Objectifs du Projet
+
+- Présenter les expositions en cours
+- Fournir des informations sur l'histoire et la mission du musée
+- Permettre aux visiteurs de contacter le musée
+- Offrir une expérience utilisateusr agréable et intus
+
+## Références
+
+- [Next.js Documentation](https://nextjs.org/docs)
+- [React Documentation](https://reactjs.org/docs/getting-started.html)
+- [W3Schools](https://www.w3schools.com/)
+- [MDN Web Docs](https://developer.mozilla.org/en-US/)
+- [React Icons](https://react-icons.github.io/react-icons/)
+- [Icons8](https://icons8.com/icons/set/museum)
+
+## Couleurs Utilisées
+
+Pour le site web du Musée de la Civilisation, nous avons choisi un thème aux couleurs gris, jaune, noir et blanc. Le jaune vibrant apporte une touche d'énergie et de créativité, tandis que le gris ajoute une élégance sophistiquée. Le noir garantit une lisibilité optimale et crée des contrastes forts, et le blanc offre une présentation claire et épurée. Cette palette de couleurs crée une expérience visuelle moderne, engageante et accessible pour tous les visiteurs du site.
+
+- **Primaire** : #333333 (Gris foncé)
+- **Secondaire** :#FFCC00 (Jaune)
+- **Fond** : #FFFFFF (Blanc)
+- **Texte** : #000000 (Noir)
+
+
+## Structure du Projet
+
+```plaintext
+- app
+    - globals.css
+    - Layout.jsx
+    - Layout.module.css
+    - page.jsx
+    - page.module.css
+
+
+- components
+    - Accueil.jsx
+    - Accueil.module.css
+    - Citation.jsx
+    - Citation.module.css
+    - Header.jsx
+    - Header.module.css
+    - Footer.jsx
+    - Footer.module.css
+    - MenuNav.jsx
+    - MenuNav.module.css
+
+  - docs
+    - preparation.md
+
+  
+   
