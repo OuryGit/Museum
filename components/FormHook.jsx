b2f05fbb1ade72f1e3f7690e41b1d3bf98b5de9d
DOUMBIA-AMADOU <161505239+DOUMBIA-AMADOU@users.noreply.github.com>
2024-06-19 00:55:29 -0400

    Revert "UpdatePusher"
    This reverts commit 30f8072bfb665c2f9bdc758bc8e68cb714551965.

--- components/FormHook.jsx
+++ components/FormHook.jsx
@@ -1,15 +1,12 @@
 'use client';
-import React, { useRef, useState } from 'react';
 import styles from './Form.module.css';
 import { useForm } from 'react-hook-form';
-import emailjs from 'emailjs-com';
 
 export default function FormHook() {
-    const [infosResults, setInfosResults] = useState('');
-    const formRef = useRef(null);
     const {
         register,
         handleSubmit,
+        watch,
         formState: { errors }
     } = useForm({
         defaultValues: {
@@ -19,45 +16,23 @@ export default function FormHook() {
         }
     });
 
-    const onSubmit = (data) => {
-        const formData = new FormData(formRef.current);
-
-        const templateParams = {
-            name: formData.get('nom'),
-            email: formData.get('courriel'),
-            message: formData.get('message'),
-        };
-
-        emailjs.send(
-            'service_13ynu28',
-            'template_ktqkbq9',
-            templateParams,
-            'rIqVAJf8O17e75r0O'
-        ).then(
-            (response) => {
-                console.log('SUCCESS!', response.status, response.text);
-                setInfosResults('Votre message a été envoyé avec succès.');
-            },
-            (error) => {
-                console.error('FAILED...', error);
-                setInfosResults('Une erreur est survenue lors de l\'envoi de votre message.');
-            }
-        );
+    const onSubmit = data => { console.log(data);
+       
     };
 
     return (
         <>
-            <form ref={formRef} className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+            <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
                 <label className={styles.label}>
                     Nom:
                     <input
                         type="text"
                         className={styles.input}
                         {...register("nom", {
-                            required: 'Ce champ doit être rempli',
-                            minLength: { value: 4, message: "Min 4 caractères" }
+                            required: 'ce champ doit être rempli ',
+                            minLength: { value: 4, message: "min 4 caractères" }
                         })}
-                        placeholder='Votre nom'
+                        placeholder='votre nom'
                     />
                     <div className={styles.erreur}>{errors.nom?.message}</div>
                 </label>
@@ -67,13 +42,13 @@ export default function FormHook() {
                         type="email"
                         className={styles.input}
                         {...register("courriel", {
-                            required: 'Ce champ doit être rempli',
+                            required: 'ce champ doit être rempli',
                             pattern: {
                                 value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
                                 message: 'Courriel non valide'
                             }
                         })}
-                        placeholder='Votre courriel'
+                        placeholder='votre courriel'
                     />
                     <div className={styles.erreur}>{errors.courriel?.message}</div>
                 </label>
@@ -82,15 +57,14 @@ export default function FormHook() {
                     <textarea
                         className={styles.textarea}
                         {...register("message", {
-                            required: 'Ce champ doit être rempli'
+                            required: 'ce champ doit être rempli'
                         })}
-                        placeholder='Votre message'
+                        placeholder='votre message'
                     ></textarea>
                     <div className={styles.erreur}>{errors.message?.message}</div>
                 </label>
                 <button type="submit" className={styles.button}>Envoyer</button>
             </form>
-            {infosResults && <p className={styles.result}>{infosResults}</p>}
         </>
     );
 }
